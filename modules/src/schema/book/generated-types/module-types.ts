5d76d9f44dd1dc41b1a00b3050d97d9c91a1e7c7
izumin5210 <m@izum.in>
2023-12-16 23:25:03 +0900

    add modules sample

---
+++ modules/src/schema/book/generated-types/module-types.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+import * as Types from "../../../generated-types/graphql";
+import * as gm from "graphql-modules";
+export namespace BookModule {
+  interface DefinedFields {
+    Query: 'book';
+    Mutation: 'markBookAsRead';
+    Book: 'id' | 'isbn';
+  };
+  
+  export type Query = Pick<Types.Query, DefinedFields['Query']>;
+  export type Book = Pick<Types.Book, DefinedFields['Book']>;
+  export type Mutation = Pick<Types.Mutation, DefinedFields['Mutation']>;
+  
+  export type QueryResolvers = Pick<Types.QueryResolvers, DefinedFields['Query']>;
+  export type MutationResolvers = Pick<Types.MutationResolvers, DefinedFields['Mutation']>;
+  export type BookResolvers = Pick<Types.BookResolvers, DefinedFields['Book'] | '__isTypeOf'>;
+  
+  export interface Resolvers {
+    Query?: QueryResolvers;
+    Mutation?: MutationResolvers;
+    Book?: BookResolvers;
+  };
+  
+  export interface MiddlewareMap {
+    '*'?: {
+      '*'?: gm.Middleware[];
+    };
+    Query?: {
+      '*'?: gm.Middleware[];
+      book?: gm.Middleware[];
+    };
+    Mutation?: {
+      '*'?: gm.Middleware[];
+      markBookAsRead?: gm.Middleware[];
+    };
+    Book?: {
+      '*'?: gm.Middleware[];
+      id?: gm.Middleware[];
+      isbn?: gm.Middleware[];
+    };
+  };
+}
